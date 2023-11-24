d7c91b21e9ec1f0d5e1e281058a898142771a3af
Amenallah28 <amenallahmrabet9@gmail.com>
2023-11-24 15:08:08 +0100

    header file

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define __PRINTF__
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
@@ -11,11 +12,13 @@ typedef struct amen
 	int (*fun)(va_list);
 }ahla_nes;
 
-int (*print(char specifier)(va_list args));
+int (*print(char specifier))(va_list args);
 int print_char(va_list arg);
 int print_str(va_list arg);
 int print_numb(va_list arg);
 int print_numb_recursive(int number);
+int _putchar(char c);
+int _printf(const char *format, ...);
 
 
 #endif /*__PRINTF__*/
