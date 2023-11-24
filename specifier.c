e8cdcdc7f28cb283f78a7f7c3ee13cacf3f3fffe
amenallah122 <garmaiamenallah@gmail.com>
2023-11-24 13:40:15 +0100

    specifier

---
+++ specifier.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_format -  selects the right function
+ * @specifier: the character
+ * Return: 0
+ */
+int print_format(char specifier, va_list args)
+{
+	ahla_nes fel_kayes[] = {
+		{'c', print_char},
+		{'s', print_str},
+		{'d', print_numb},
+		{'\0', NULL}
+	};
+	int j;
+
+	for ( j = 0; fel_kayes[j].character != '\0'; j++)
+	{
+		if (fel_kayes[j].character == specifier)
+		{
+			return (fel_kayes.fun);
+		}
+	}
+	return (0);
+}
