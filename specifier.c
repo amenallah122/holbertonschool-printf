218d635f2739373a0264795599b0eda91c9604e7
Amenallah28 <amenallahmrabet9@gmail.com>
2023-11-26 13:05:08 +0100

    trying to fix the %! and %K

--- specifier.c
+++ specifier.c
@@ -4,23 +4,24 @@
  * @specifier: the character
  * Return: 0
  */
-int (*print( char specifier))(va_list)
+int (*print(char specifier))(va_list)
 {
 	ahla_nes fel_kayes[] = {
 		{'c', print_char},
 		{'s', print_str},
 		/*{'d', print_numb},
 		//{'i', print_numb},*/
-		{'\0', NULL}
-	};
+		{'\0', NULL}};
 	int j;
 
-	for ( j = 0; fel_kayes[j].character != '\0'; j++)
+	for (j = 0; fel_kayes[j].character != '\0'; j++)
 	{
 		if (fel_kayes[j].character == specifier)
 		{
 			return (fel_kayes[j].fun);
 		}
 	}
-	return (0);
+	_putchar('%');
+	_putchar(specifier);
+	return (2);
 }
