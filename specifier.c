64512727b273b5cf3a513a3acac8c073fcfdb89b
amenallah122 <garmaiamenallah@gmail.com>
2023-11-26 14:35:24 +0100

    specifier.c

--- specifier.c
+++ specifier.c
@@ -9,8 +9,8 @@ int (*print( char specifier))(va_list)
 	ahla_nes fel_kayes[] = {
 		{'c', print_char},
 		{'s', print_str},
-		/*{'d', print_numb},
-		//{'i', print_numb},*/
+		{'d', print_numb},
+		{'i', print_numb},
 		{'\0', NULL}
 	};
 	int j;
